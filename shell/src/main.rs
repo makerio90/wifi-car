e1c9829166836bc4a28d42b2f909887eda9be421
makerio90 <makerio1@pm.me>
2022-09-10 15:34:43 -0400

    start of shell

--- shell/src/main.rs
+++ shell/src/main.rs
@@ -1,3 +1,10 @@
+use drivers::Drivers;
+use std::env;
+
 fn main() {
+    println!(
+        "Shell baised driver interface v{}",
+        env!("CARGO_PKG_VERSION")
+    );
     println!("Hello, world!");
 }
