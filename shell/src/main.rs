fb9c915d5741caa087395a909381e7aaac02d849
makerio90 <makerio1@pm.me>
2022-09-10 17:27:26 -0400

    some changes

--- shell/src/main.rs
+++ shell/src/main.rs
@@ -1,10 +1,22 @@
 use drivers::Drivers;
+use shrust::{Shell, ShellIO};
 use std::env;
+use std::io::Write;
 
 fn main() {
     println!(
         "Shell baised driver interface v{}",
         env!("CARGO_PKG_VERSION")
     );
-    println!("Hello, world!");
+    let mut driver: Option<Drivers> = None;
+
+    let mut shell = Shell::new(driver);
+    shell.new_command("set", "set the driver", 1, |io, driver, select| {
+        driver = match select[0] {
+            "demo" => Some(Drivers::Demo()),
+        };
+        Ok(())
+    });
+
+    shell.run_loop(&mut ShellIO::default());
 }
