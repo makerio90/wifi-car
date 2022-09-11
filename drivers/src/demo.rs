9747258361c773e7412da87c27d11dd5ec1e9211
makerio90 <makerio1@pm.me>
2022-09-11 16:17:19 -0400

    forgot to track demo file

---
+++ drivers/src/demo.rs
@@ -0,0 +1,50 @@
+use crate::{Driver, DriverError, Result};
+
+/// demo driver for testing
+pub struct Demo {
+    enabled: bool,
+}
+impl Driver for Demo {
+    fn enable(&mut self) -> Result<()> {
+        self.enabled = true;
+        println!("dummy driver enabled!");
+        Ok(())
+    }
+    fn is_ready(&self) -> bool {
+        self.enabled
+    }
+    fn drive(&mut self, accelerate: f64, steer: f64) -> Result<()> {
+        if !(0.0..1.0).contains(&accelerate) || !(-1.0..1.0).contains(&steer) {
+            return Err(DriverError::OutOfRange);
+        }
+        if self.enabled {
+            return Err(DriverError::NotEnabled);
+        }
+        let speed = accelerate * 100.0;
+        let dir = if steer.is_sign_negative() {
+            "left"
+        } else {
+            "right"
+        };
+        let amount = steer.abs() * 100.0;
+        println!(
+            "sent command to drive at speed {}% and steer {} {}",
+            speed, dir, amount
+        );
+        Ok(())
+    }
+    fn estop(&mut self) -> Result<()> {
+        println!("estop pulled");
+        Ok(())
+    }
+    fn has_break(&self) -> bool {
+        true
+    }
+    fn is_proportional(&self) -> (bool, bool) {
+        (true, true)
+    }
+    fn disable(&mut self) -> Result<()> {
+        println!("disabled");
+        Ok(())
+    }
+}
