69aca758939f95f50cded0e04c91e8a3bfe5554c
makerio90 <makerio1@pm.me>
2022-09-17 13:35:06 -0400

    more changes

--- drivers/src/drivers/simple_skid_steer.rs
+++ drivers/src/drivers/simple_skid_steer.rs
@@ -116,7 +116,7 @@ impl Driver for SkidSteer {
 
         let left = (accelerate - steer).clamp(-1.0, 1.0);
         let right = (accelerate + steer).clamp(-1.0, 1.0);
-        println!("{:?}", self);
+        println!("{:#?}", self);
         self.rva
             .as_mut()
             .ok_or(DriverError::ExpectedSomeFoundNone)?
