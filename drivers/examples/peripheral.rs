09a6cd583b2a9034f63dacec08ce39bbd19dfd86
makerio90 <makerio1@pm.me>
2022-11-26 17:31:26 -0500

    small but important change

--- drivers/examples/peripheral.rs
+++
@@ -1,7 +0,0 @@
-use drivers::peripheral::{Example, Peripheral};
-
-fn main() {
-    let per = Example::init();
-    println!("status: {:?}", per.status());
-    println!("indata: {:?}", Example::InData);
-}
